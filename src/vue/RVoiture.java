e7fb3d4314ce12ebabe81a296ed399096a6a4aed
Lucas <lucas.portal@orange.fr>
2020-05-18 18:39:05 -0400

    avancement RVoitures

--- src/vue/RVoiture.java
+++ src/vue/RVoiture.java
@@ -1,4 +1,5 @@
 package vue;
+import donnees.BD;
 /**
  * Rapport des voitures.
  * 
@@ -6,7 +7,24 @@ package vue;
  *
  */
 public class RVoiture {
+	
 	public RVoiture() {
+		
+		int i;
+		BD bd = new BD();
+		String voiture=""; 
+		String listeVoitures="";
+		
+		for (i=0; i<bd.arrLstVoiture.size(); i++) {
+			
+			voiture = bd.arrLstVoiture.get(i).getModele();
+			voiture += " Marque : " + bd.arrLstVoiture.get(i).getMarque();
+			voiture += " Annee : " + bd.arrLstVoiture.get(i).getAnnee();	
+			listeVoitures += voiture;
+		}	
+		System.out.println("Rapport voitures: " + listeVoitures);
+	
 		
 	}
 }
+
